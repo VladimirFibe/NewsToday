1457441a9b536851b303f19eb1a3a6974bd2d5d7
Vladimir Fibe <motiw@me.com>
2024-10-25 08:12:53 +0500

    bookmark

---
+++ NewToday/API/APIError.swift
@@ -0,0 +1,13 @@
+//
+//  APIError.swift
+//  NewsToday
+//
+//  Created by Vladimir Fibe on 20.10.2024.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case failedRequest
+    case failedTogetData
+}
