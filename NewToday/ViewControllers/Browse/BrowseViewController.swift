5219ddfe3f3f064becfe611eb0ed573878ba75ae
Vika <vg.dev31@gmail.com>
2024-10-25 18:33:09 +0400

    Add extension for header

--- NewToday/ViewControllers/Browse/BrowseViewController.swift
+++ NewToday/ViewControllers/Browse/BrowseViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class BrowseViewController: UIViewController {
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        setupHeaderView()
     }
+    
+    private func setupHeaderView() {
+            // Используем расширение для добавления хедера
+            view.addHeader(
+                title: "Browse",
+                subTitle: "Discover things of this world"
+            )
+        }
 }
