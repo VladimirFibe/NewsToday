32db972f9acaf560c1e5c407e3156b798aa454ce
Nadezhda <el.shtolts@yandex.ru>
2024-10-27 23:23:15 +0300

    changed rootvc

--- NewsToday/App/SceneDelegate.swift
+++ NewsToday/App/SceneDelegate.swift
@@ -57,7 +57,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return
         }
         
-        window.rootViewController = ArticleViewController()
+        window.rootViewController = controller
         window.makeKeyAndVisible()
         UIView.transition(with: window,
                           duration: 0.3,
