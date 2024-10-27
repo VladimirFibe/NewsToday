3b3275f31812b2b2e23229116e3e74f2724a33c1
Nadezhda <el.shtolts@yandex.ru>
2024-10-27 19:42:43 +0300

    intermediate commit

--- NewsToday/ViewControllers/ArticlePage/ArticleViewController.swift
+++ NewsToday/ViewControllers/ArticlePage/ArticleViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ArticleViewController: UIViewController {
     
-    // MARK: - Property
 
-    private let articleImageView = UIImageView()
+    
+    //MARK: - Private properties
+    
+    //var data = [Article]() // здесь будут данные из интернета
+    var tag: String = "Politic"
+    var isArticleSaved = true
+    
+    //MARK: - UI Components
+
+    private let scrollView: UIScrollView = {
+        let element = UIScrollView()
+        element.showsVerticalScrollIndicator = true
+        return element
+    }()
+    
+    private let contentView: UIView = {
+        let element = UIView()
+        return element
+    }()
+    
+    private let articleImageView: UIImageView = {
+        let element = UIImageView()
+        element.contentMode = .scaleAspectFill
+        return element
+    }()
     
     private let blackView: UIView = {
         let element = UIView()
@@ -46,8 +69,9 @@ class ArticleViewController: UIViewController {
         let element = UILabel()
         element.font = UIFont(name: "Inter-Regular", size: 14)
 
-        element.textColor =  .white
+        element.textColor =  .black
         element.text = "Author"
+
         element.numberOfLines = 0
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
@@ -57,49 +81,69 @@ class ArticleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-
         setupView()
 
     }
     func setupView() {
+//        scrollView = UIScrollView(frame: self.view.bounds)
         
-        view.addSubview(articleImageView)
-        view.addSubview(blackView)
-        view.addSubview(titleLabel)
-        view.addSubview(nameLabel)
-        view.addSubview(authorLabel)
-    //настройка картинки
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(articleImageView)
+        contentView.addSubview(blackView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(authorLabel)
+    
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.contentSize = self.contentView.bounds.size
+        //настройка картинки
         
         articleImageView.translatesAutoresizingMaskIntoConstraints = false
         articleImageView.image = UIImage(named: "samplePolitics")
-        articleImageView.contentMode = .scaleAspectFill
+
         
         blackView.translatesAutoresizingMaskIntoConstraints = false
+
         
 
         
         
         
         NSLayoutConstraint.activate([
-            articleImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            articleImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            articleImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            articleImageView.heightAnchor.constraint(equalToConstant: 370),
-        
-            blackView.topAnchor.constraint(equalTo: view.topAnchor),
-            blackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            blackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            blackView.heightAnchor.constraint(equalToConstant: 370),
-        
-            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 216),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             
-            nameLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
-            nameLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-            
-            authorLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 6),
-            authorLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor)
+//            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+//            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+//           
+//            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+//            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+//            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+//            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+//
+//            
+//            articleImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+//            articleImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+//            articleImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+//            articleImageView.heightAnchor.constraint(equalToConstant: 370),
+//        
+//            blackView.topAnchor.constraint(equalTo: articleImageView.topAnchor),
+//            blackView.trailingAnchor.constraint(equalTo: articleImageView.trailingAnchor),
+//            blackView.leadingAnchor.constraint(equalTo: articleImageView.leadingAnchor),
+//            blackView.heightAnchor.constraint(equalTo: articleImageView.heightAnchor),
+//        
+//            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 216),
+//            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+//            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+//            
+//            nameLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+//            nameLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+//            
+//            authorLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 6),
+//            authorLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+
         
         ])
     }
